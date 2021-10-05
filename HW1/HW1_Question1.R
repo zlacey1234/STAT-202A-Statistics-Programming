d483fc71210cf5a6e56bdad7ee509cc3f308198f
Zachary Lacey <zlacey1234@gmail.com>
2021-10-05 00:01:29 -0700

    Update HW1_Question1.R

--- HW1/HW1_Question1.R
+++ HW1/HW1_Question1.R
@@ -13,129 +13,134 @@
 
 library(Rcpp)
 
-
 ## 1. Assessing estimates of the 90th percentile of 100 independent, 
 ##    identically-distributed (iid) uniform (0, 1) random variables. 
 ##
-##    The R function quantile() implements a somewhat complex interpolation 
-##    method in order to estimate a particular quantile, such as the 90th 
-##    percentile. We will compare the estimate in quantile() with simpler 
-##    estimates. 
+##    The R function quantile() implements a somewhat complex 
+##    interpolation method in order to estimate a particular quantile, 
+##    such as the 90th percentile. We will compare the estimate in 
+##    quantile() with simpler estimates. 
 
-## a. Write a function that takes as input a vector of length 100 and outputs
-##    the 90th of the 100 values sorted from smallest to largest. Note that the 
-##    input vector may not be sorted.
+## a. Write a function that takes as input a vector of length 100 and 
+##    outputs the 90th of the 100 values sorted from smallest to largest. 
+##    Note that the input vector may not be sorted.
 
-## Function that sorts the sample vector and returns the nth element of the 
-## sorted vector. 
-compute_n_sample <- function(sample_vector, n) {
-  sorted_sample_vector <- sort(sample_vector)
+## Function that sorts the sample vector and returns the nth element of 
+## the sorted vector. 
+computeNSample = function(sampleVector, n) {
+  sortedSampleVector = sort(sampleVector)
   
-  sorted_sample_vector[n]
+  sortedSampleVector[n]
 }
 
 ## Function which returns the 90th element of the sorted vector.
-compute_90th <- function(sample_vector) {
-  compute_n_sample(sample_vector, 90)
+compute90th = function(sampleVector) {
+  computeNSample(sampleVector, 90)
 }
 
-
-## b. Write a function to find the 91st of the sorted vector of 100 values. 
+## b. Write a function to find the 91st of the sorted vector of 100 
+##    values. 
 
 ## Function which returns the 91st element of the sorted vector.
-compute_91st <- function(sample_vector) {
-  compute_n_sample(sample_vector, 91)
+compute91st = function(sampleVector) {
+  computeNSample(sampleVector, 91)
 }
 
-
-## c. Write a function that outputs the average of the 90th and 91st of the 
-##    sorted vector of 100 values.
+## c. Write a function that outputs the average of the 90th and 91st of 
+##    the sorted vector of 100 values.
 
 ## Function which returns the average of the 90th and 91st element of the 
 ## sorted vector.
-compute_avg_90th_91st <- function(sample_90th_current_calculation, 
-                                  sample_91st_current_calculation) {
-  (sample_90th_current_calculation + sample_91st_current_calculation) / 2
+computeAvg90th91st = function(sample90thCurrentCalculation, 
+                              sample91stCurrentCalculation) {
+  (sample90thCurrentCalculation + sample91stCurrentCalculation) / 2
 }
-
 
 ## d. For each of your function in part a - c, as well as the function 
 ##    quantile(x, 0.9), do the following:
 ##
-##    (i).   Generate 100 independent, identically-distributed (iid) uniform 
-##           (0, 1) random variables, and calculate your estimates of the 90th
-##           percentile. 
+##    (i).   Generate 100 independent, identically-distributed (iid) 
+##           uniform (0, 1) random variables, and calculate your estimates 
+##           of the 90th percentile. 
 ##
 ##    (ii).  Repeat step (i) 100,000 times.
 ##
 ##    (iii). Plot the sample mean of the first 'm' of your estimates, as a 
 ##           function of m.
+nIterations = 100000
 
-n_iterations <- 100000
+mIterations = 100000
 
-m_iterations <- 100000
+computedSampleData  = matrix(nrow = 4, ncol = nIterations)
 
-computed_sample_data  <- matrix(nrow = 4, ncol = n_iterations)
+iterations = 1:nIterations
 
-iterations <- 1:n_iterations
-
-compute_sample_n <- function(iterations_idx) {
-  sample_iid_data <- runif(100)
+computeSampleN = function(iterationsIdx) {
+  sampleIidData = runif(100)
   
-  c1 <- compute_90th(sample_iid_data)
-  c2 <- compute_91st(sample_iid_data)
-  c3 <- compute_avg_90th_91st(c1, c2)
-  c4 <- quantile(sample_iid_data, 0.9)
+  c1 = compute90th(sampleIidData)
+  c2 = compute91st(sampleIidData)
+  c3 = computeAvg90th91st(c1, c2)
+  c4 = quantile(sampleIidData, 0.9)
   
   return(c(c1, c2, c3, c4))
 }
 
 # Start the clock!
-ptm <- proc.time()
+ptm = proc.time()
 
-computed_sample_data <- sapply(iterations, compute_sample_n)
+computedSampleData = sapply(iterations, computeSampleN)
 
 # Stop the clock
 proc.time() - ptm
 
-sample_mean_90th_n <- vector("numeric", m_iterations)
-sample_mean_91st_n <- vector("numeric", m_iterations)
-sample_mean_90th_91st_n <- vector("numeric", m_iterations)
-sample_mean_90th_quantile_n <- vector("numeric", m_iterations)
+sampleMean90thN = vector("numeric", mIterations)
+sampleMean91stN = vector("numeric", mIterations)
+sampleMean90th91stN = vector("numeric", mIterations)
+sampleMean90thQuantileN = vector("numeric", mIterations)
+
+sourceCpp("sample_mean_c.cpp")
 
 # Start the clock!
-ptm <- proc.time()
+ptm = proc.time()
 
-sample_mean_90th_n = sample_mean_c(
-  as.double(computed_sample_data[1, ]),
-  m_iterations)
+sampleMean90thN = sample_mean_c(
+  as.double(computedSampleData[1, ]),
+  mIterations)
 
-sample_mean_91st_n = sample_mean_c(
-  as.double(computed_sample_data[2, ]),
-  m_iterations)
+sampleMean91stN = sample_mean_c(
+  as.double(computedSampleData[2, ]),
+  mIterations)
 
-sample_mean_90th_91st_n = sample_mean_c(
-  as.double(computed_sample_data[3, ]),
-  m_iterations)
+sampleMean90th91stN = sample_mean_c(
+  as.double(computedSampleData[3, ]),
+  mIterations)
 
-sample_mean_90th_quantile_n = sample_mean_c(
-  as.double(computed_sample_data[4, ]),
-  m_iterations)
+sampleMean90thQuantileN = sample_mean_c(
+  as.double(computedSampleData[4, ]),
+  mIterations)
 # Stop the clock
 proc.time() - ptm
 
 
 
-plot_sample_mean <- function(m_iter) {
-  plot(1:m_iter, sample_mean_90th_n, pch=".", col="red", 
+plot_sample_mean = function(m_iter) {
+  plot(1:m_iter, sampleMean90thN, pch=".", col="red", 
        xlab="First m iterations", ylab="Sample Mean")
   
-  points(1:m_iter, sample_mean_91st_n, pch=".", col="blue")
-  points(1:m_iter, sample_mean_90th_91st_n, pch=".", col="green")
-  points(1:m_iter, sample_mean_90th_quantile_n, pch=".")
+  points(1:m_iter, sampleMean91stN, pch=".", col="blue")
+  points(1:m_iter, sampleMean90th91stN, pch=".", col="green")
+  points(1:m_iter, sampleMean90thQuantileN, pch=".")
+  
+  legend(50000, 0.915, 
+         legend = c("Compute 90th", "Compute 91st", 
+                    "Average 90th and 91st", "Compute Quantile"),
+         cex = 0.8,
+         col = c("red", "blue", "green", "black"),
+         pch = c(".",".",".","."))
 }
 
-plot_sample_mean(m_iterations)
+plot_sample_mean(mIterations)
 
 
 ## e. Report the ultimate sample mean of your 100,000 estimates, for each of 
@@ -143,17 +148,12 @@ plot_sample_mean(m_iterations)
 ##    appears to be the best, and why.
 
 print(c("Computed 90th of Sorted:",
-        sample_mean(computed_sample_data[1, ],
-                    n_iterations)))
+        sampleMean90thN[nIterations]))
 print(c("Computed 91st of Sorted:",
-        sample_mean(computed_sample_data[2, ],
-                    n_iterations)))
+        sampleMean91stN[nIterations]))
 print(c("Computed Average 90th and 91st:",
-        sample_mean(computed_sample_data[3, ],
-                    n_iterations)))
+        sampleMean90th91stN[nIterations]))
 print(c("Computed 90th from Quantile:",
-        sample_mean(computed_sample_data[4, ],
-                    n_iterations)))
+        sampleMean90thQuantileN[nIterations]))
 
 print("Finished")
-
