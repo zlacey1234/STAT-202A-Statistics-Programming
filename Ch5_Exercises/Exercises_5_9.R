730a346c36242177e9a7ea52972853fb79dbce3c
Zachary Lacey <zlacey1234@gmail.com>
2021-10-10 19:09:47 -0700

    Create Exercises_5_9.R

---
+++ Ch5_Exercises/Exercises_5_9.R
@@ -0,0 +1,7 @@
+## Exercises 5.9
+
+## 1. Pick a measurement you can take on a regular basis. For example, 
+##    your daily weight or how long it takes you to run 5 miles. Keep a 
+##    spreadsheet that includes the date, the hour, the measurement, and 
+##    any other informative variable you think is worth keeping. Do this 
+##    for two weeks. Then make a plot.
