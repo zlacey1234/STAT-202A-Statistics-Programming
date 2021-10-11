ad34245ea8d69e7c84c4b33f899929846b2d780c
Zachary Lacey <zlacey1234@gmail.com>
2021-10-11 00:50:45 -0700

    Create Exercises_8_7.R
    Exercises 8.7

---
+++ Ch8_Exercises/Exercises_8_7.R
@@ -0,0 +1,147 @@
+## Exercises 8.7
+
+## 1. In the 'murders' dataset, the region is a categorical variable and 
+##    the following is its distribution: 
+##
+##    [See Textbook for Graph] 
+##    https://rafalab.github.io/dsbook/distributions.html#exercises-16]
+## 
+##    To the closest 5%, what proportion of the sates are in the North 
+##    Central region?
+##
+##    First Guess Answer: ~ 25%
+##    Result: 
+
+
+## 2. Which of the following is true:
+##
+##    a. The graph above is a histogram. 
+##    b. The graph above shows only four members with a bar plot.
+##    c. Categories are not numbers, so it does not make sense to graph 
+##       the distribution.
+##    d. The colors, not the height of the bars, describe the distribution.
+##
+##    First Guess Answer: b
+##    Result:
+
+
+## 3. The plot below shows the eCDF for male heights:
+##
+##    [See Textbook for Graph] 
+##    https://rafalab.github.io/dsbook/distributions.html#exercises-16]
+##
+##    Based on the plot, what percentage of males are shorter than 75 
+##    inches?
+##
+##    a. 100% 
+##    b. 95%
+##    c. 80%
+##    d. 72 inches
+##
+##    First Guess Answer: b
+##    Result:
+
+
+## 4. To the closest inch, what height 'm' has the property that 1/2 of 
+##    the male students are taller than 'm' and 1/2 are shorter?
+##    
+##    a. 61 inches
+##    b. 64 inches
+##    c. 69 inches
+##    d. 74 inches
+##
+##    First Guess Answer: c
+##    Result:
+
+
+## 5. Here is an eCDF of the murder rates across states:
+##
+##    [See Textbook for Graph] 
+##    https://rafalab.github.io/dsbook/distributions.html#exercises-16]
+##
+##    Knowing that there are 51 states (counting DC) and based on this 
+##    plot, how many states have murder rates larger than 10 per 100,000 
+##    people?
+##
+##    a. 1
+##    b. 5
+##    c. 10
+##    d. 50
+##  
+##    First Guess Answer: 
+##    Result: 
+
+
+## 6. Based on the eCDF above, which of the following statements are true:
+##
+##    a. About half the states have murder rates above 7 per 100,000 and 
+##       the other half below.
+##    b. Most states have murder rates below 2 per 100,000.
+##    c. All the states have murder rates above 2 per 100,000.
+##    d. With the exception of 4 states, the murder rates are below 5 per 
+##       100,000.
+##  
+##    First Guess Answer: 
+##    Result: 
+
+
+## 7. Below is a histogram of male heights in our 'heights' dataset:
+##
+##    [See Textbook for Graph] 
+##    https://rafalab.github.io/dsbook/distributions.html#exercises-16]
+##
+##    Based on this plot, how many males are between 63.5 and 65.5?
+##    
+##    a. 10
+##    b. 24
+##    c. 34
+##    d. 100
+##
+##    First Guess Answer: c
+##    Result:
+
+
+## 8. About what percentage are shorter than 60 inches?
+##
+##    a.  1%
+##    b. 10%
+##    c. 25%
+##    d. 50%
+##
+##    First Guess Answer: a
+##    Result: 
+
+
+## 9. Based on the density plot below, about what proportion of US states 
+##    have populations larger than 10 million?
+##
+##    [See Textbook for Graph] 
+##    https://rafalab.github.io/dsbook/distributions.html#exercises-16]
+##
+##    a. 0,02
+##    b. 0.15
+##    c. 0.50
+##    d. 0.55
+##  
+##    First Guess Answer: 
+##    Result: 
+
+
+## 10. Below are three density plots. Is it possible that they are from 
+##     the same dataset?
+##
+##    [See Textbook for Graph] 
+##    https://rafalab.github.io/dsbook/distributions.html#exercises-16]
+##
+##    Which of the following statements is true:
+##
+##    a. It is impossible that they are from the same dataset.
+##    b. They are from the same dataset, but the plots are different due 
+##       to code errors.
+##    c. They are the same dataset, but the first and second plot 
+##       under-smooth and the third over-smooths.
+##    d. They are the same dataset, but the first is not in the log scale, 
+##       the second under-smooths, and the third over-smooths.
+##  
+##    First Guess Answer: 
+##    Result: 
