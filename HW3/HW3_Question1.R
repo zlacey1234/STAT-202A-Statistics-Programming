2e49846768b1bc400749954b23181a9cfa00110e
Zachary Lacey <zlacey1234@gmail.com>
2021-10-28 02:24:23 -0700

    Create HW3_Question1.R
    Basically finished Question 1

---
+++ HW3/HW3_Question1.R
@@ -0,0 +1,53 @@
+## Homework 3 Question 1
+##
+## Author: Zachary G. Lacey
+## Email: zlacey@g.ucla.edu
+## Date: October 26th, 2021
+##
+## Course: STAT 202A - Statistics Programming
+## Assignment: Homework 3
+## Due Date: 
+##
+## Question 1
+##
+
+library(Rcpp)
+library(tidyverse)
+
+## 1. Approximation of an infinite series in C. 
+## 
+##    It is well known that 
+##   
+##    1 - 1/2 + 1/3 - 1/4 + 1/5 - 1/6 +/- ... = ln(2). 
+##
+##    Write a C function called alt2(n) that computes the first 'n' terms
+##    in this series, as a function of 'n'. Call your C function from R 
+##    to evaluate alt2(n) for various 'n'. Using R, plot alt2(n) vs. 'n',
+##    for 'n' ranging from some small number up to 1 million. You may set 
+##    up your range of the y-axis in a way that you feel is appropriate. 
+##    You do not need to show alt2(n) for all values of n and should not 
+##    plot alt2 for very small values of 'n' if they are off the plot.
+
+sourceCpp("alt2.cpp")
+
+nIterations = 1000000
+
+alt2Estimates = vector("numeric", nIterations)
+
+alt2Estimates = alt2(1000000)
+
+nSeq = seq(1000, nIterations, 1000)
+
+alt2EstimatesSeq = alt2Estimates[nSeq]
+
+print(nSeq)
+print(alt2EstimatesSeq)
+
+alt2EstimatesTibble = tibble(n = nSeq, 
+                             alt2Estimates = alt2EstimatesSeq)
+
+
+alt2EstimatesTibble %>% ggplot() +
+  geom_point(aes(x = n, y = alt2Estimates))
+
+print("Finished")
