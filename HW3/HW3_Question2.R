1601cd61884aa95f719f60c82f9494e7afac516b
Zachary Lacey <zlacey1234@gmail.com>
2021-11-09 15:18:47 -0800

    Update HW3_Question2.R
    Fix comments to not exceed horizontal rule (75 spaces)

--- HW3/HW3_Question2.R
+++ HW3/HW3_Question2.R
@@ -17,28 +17,29 @@ library(tidyverse)
 ## 2. Kernel density estimation in C and plotted in R.
 ##
 ##    Write a C function to compute a Gaussian kernel density estimate for 
-##    uni-variate data. The inputs to the function should be two integers, $m$ 
-##    and $n$, a vector $g$ of $m$ gridpoints at which to calculate the 
-##    estimates, a vector $x$ consisting of the $n$ observed data points, and 
-##    a vector $y$ of length $m$ which will contain the resulting density 
-##    estimates.
+##    uni-variate data. The inputs to the function should be two integers, 
+##    $m$ and $n$, a vector $g$ of $m$ gridpoints at which to calculate 
+##    the estimates, a vector $x$ consisting of the $n$ observed data 
+##    points, and a vector $y$ of length $m$ which will contain the 
+##    resulting density estimates.
 ##
-##    Gather data on all earthquakes of magnitude at least 3.0 in the longitude 
-##    range -122.0 to -118.0 and latitude range 34.0 to 38.0, from Jan 1, 1960 
-##    to Oct 1, 2021, from 
+##    Gather data on all earthquakes of magnitude at least 3.0 in the 
+##    longitude range -122.0 to -118.0 and latitude range 34.0 to 38.0, 
+##    from Jan 1, 1960 to Oct 1, 2021, from 
 ##
 ##    https://service.scedc.caltech.edu/eq-catalogs/date\_mag\_loc.php. 
 ##
-##    Input the data into R. (Use minimum magnitude = 3.0, maximum magnitude 
-##    = 9.0, min depth = -5km, max depth = 100km, event type = earthquake, 
-##    geographic type = local). Take this vector of earthquake magnitudes, 
-##    and use your C function to make a kernel density estimate of the 
-##    earthquake magnitudes, using a Gaussian kernel with bandwidth selected 
-##    using the rule of thumb suggested by Scott (1992). You may calculate 
-##    this bandwidth in R. Let $m_{1}, m_{2}, \dots, m_{100}$ = a vector of 
-##    100 equally spaced magnitudes spanning the observed range of magnitudes 
-##    in your dataset, and plot your kernel density estimate $\hat{f}(m_{1}), 
-##    \hat{f}(m_{1}), \dots, \hat{f}(m_{100})$.
+##    Input the data into R. (Use minimum magnitude = 3.0, maximum 
+##    magnitude = 9.0, min depth = -5km, max depth = 100km, event type = 
+##    earthquake, geographic type = local). Take this vector of earthquake 
+##    magnitudes, and use your C function to make a kernel density 
+##    estimate of the earthquake magnitudes, using a Gaussian kernel with 
+##    bandwidth selected using the rule of thumb suggested by Scott 
+##    (1992). You may calculate this bandwidth in R. Let $m_{1}, m_{2}, 
+##    \dots, m_{100}$ = a vector of 100 equally spaced magnitudes spanning 
+##    the observed range of magnitudes in your dataset, and plot your 
+##    kernel density estimate $\hat{f}(m_{1}), \hat{f}(m_{1}), \dots, 
+##    \hat{f}(m_{100})$.
 
 # Loading the Earthquake Data
 earthquakeData = read.table("SearchResults.txt")
