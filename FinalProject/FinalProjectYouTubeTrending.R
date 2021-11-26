7496b0b08663c4932381f17ec66a0a17bb25e99c
Zachary Lacey <zlacey1234@gmail.com>
2021-11-26 13:56:34 -0800

    Create FinalProjectYouTubeTrending.R
    Load the Data

---
+++ FinalProject/FinalProjectYouTubeTrending.R
@@ -0,0 +1,19 @@
+## Final Project 
+##
+## Author: Zachary G. Lacey
+## Email: zlacey@g.ucla.edu
+## Date: November 10th, 2021
+##
+## Course: STATS 202A - Statistics Programming
+## Assignment: Final Project
+## Due Date: November 29th, 2021
+##
+## Final Project - Youtube Trending Analytics
+## 
+
+library(Rcpp)
+library(tidyverse)
+
+## Loading Youtube Trending Analytics
+
+YoutubeTrendingAnalyticsData = read.csv("US_youtube_trending_data.csv")
